e5b2aa60d3cb70ded041e34df864d4642284eca7
Stewart Orr <stewart.orr@gmail.com>
2026-02-02 10:56:14 +0000

    chore: update languages

---
+++ core/components/smushit/lexicon/de/default.inc.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @package smushit
+ * @subpackage lexicon
+ */
+
+$_lang['smushit'] = 'SmushIt';
+$_lang['smushit.management_desc'] = 'Liste optimierter Bilder und deren Status aus dem SmushIt-Extra anzeigen.';
+$_lang['smushit.management_src'] = 'Quelle';
+$_lang['smushit.management_dest'] = 'Optimiert';
+$_lang['smushit.management_original'] = 'Original';
+$_lang['smushit.management_optimised'] = 'Optimiert';
+$_lang['smushit.management_saving'] = 'Ersparnis';
+$_lang['smushit.management_format'] = 'Format';
+$_lang['smushit.management_smushdate'] = 'Datum';
+$_lang['smushit.management_overwritten'] = 'Dieses Bild wurde mit dem optimierten Bild überschrieben, da es nicht das Original war.';
+$_lang['smushit.management_renamed'] = 'Dieses optimierte Bild wurde unter einem alternativen Namen gespeichert, da es das Original war.';
+$_lang['smushit.management_remove'] = 'Sind Sie sicher, dass Sie dieses Bild löschen möchten? Es wird erneut generiert, wenn es wieder mit SmushIt verwendet wird.';
+$_lang['smushit.error_no_id'] = 'Löschen nicht möglich, da keine ID angegeben wurde.';
+$_lang['smushit.error_not_found'] = 'Löschen nicht möglich, da die SmushIt-ID nicht gefunden wurde.';
+$_lang['smushit.desc'] = 'Bilder, die vom SmushIt-Extra optimiert wurden.';
+$_lang['smushit.qlty'] = 'Standard-Bildqualität für Bilder, die von SmushIt optimiert wurden.';
---
+++ core/components/smushit/lexicon/ru/default.inc.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @package smushit
+ * @subpackage lexicon
+ */
+
+$_lang['smushit'] = 'SmushIt';
+$_lang['smushit.management_desc'] = 'Просмотр списка оптимизированных изображений и их статуса через дополнение SmushIt.';
+$_lang['smushit.management_src'] = 'Источник';
+$_lang['smushit.management_dest'] = 'Оптимизировано';
+$_lang['smushit.management_original'] = 'Оригинал';
+$_lang['smushit.management_optimised'] = 'Оптимизировано';
+$_lang['smushit.management_saving'] = 'Экономия';
+$_lang['smushit.management_format'] = 'Формат';
+$_lang['smushit.management_smushdate'] = 'Дата';
+$_lang['smushit.management_overwritten'] = 'Это изображение было перезаписано оптимизированным, так как оно не являлось оригиналом.';
+$_lang['smushit.management_renamed'] = 'Это оптимизированное изображение было сохранено под другим именем, так как оно было оригиналом.';
+$_lang['smushit.management_remove'] = 'Вы уверены, что хотите удалить это изображение? Оно будет сгенерировано заново при повторном использовании с SmushIt.';
+$_lang['smushit.error_no_id'] = 'Невозможно удалить, так как ID не указан.';
+$_lang['smushit.error_not_found'] = 'Невозможно удалить, так как ID SmushIt не найден.';
+$_lang['smushit.desc'] = 'Изображения, оптимизированные дополнением SmushIt.';
+$_lang['smushit.qlty'] = 'Стандартное качество изображения для изображений, оптимизированных SmushIt.';
